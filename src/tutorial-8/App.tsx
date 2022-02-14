d572ddfd1f49d1f7ec1c6bea29cc0ed4028d7d56
oldbelom <oldbelom@gmail.com>
2022-02-14 15:41:37 +0300

    Сделал ДЗ №8

---
+++ src/tutorial-8/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+interface IUser {
+    id: string;
+    createdAt: Date;
+    name: string;
+    email: string;
+    phone: string;
+}
+
+function App() {
+    const [users, setUsers] = React.useState<IUser[]>([]);
+
+    const getUsers: () => void = async () => {
+        try {
+            const response = await fetch('https://620a478f92946600171c593c.mockapi.io/users');
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            const data = await response.json();
+            setUsers(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={getUsers}>Получить список пользователей</button>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
